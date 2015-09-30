6d633de3ef20309ab0d7afad4ad983c379bce8b9
Shruti Nallari <shruti.nallari@gmail.com>
2015-09-30 07:06:04 -0400

    On Key-BACK EVENT

--- app/src/main/java/com/example/shrutinallari/webapp/MainActivity.java
+++ app/src/main/java/com/example/shrutinallari/webapp/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.shrutinallari.webapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,7 +15,6 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.squareup.picasso.Picasso;
 
 public class MainActivity extends Activity {
 
